0cce0da1376510f6f1887837d754421170a448b7
Ayush Jamwal <ayushjamwal10@gmail.com>
2025-09-19 22:03:00 +0530

    fix: Correct helmet import syntax in index.ts and update tsconfig for module interoperability

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
+import { default as helmet } from "helmet";
 import dotenv from "dotenv";
 
 import { logger } from "./lib/logger.js";
